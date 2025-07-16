47320d357724e1674b70148a68b0a15c272ee38a
612-teacher001 <612-teacher001@la-study.com>
2025-07-16 21:27:24 +0900

    ver.0.10(MLS101): 商品の全検索（SQLの定数化）

--- src/app/ProductListDemo.java
+++ src/app/ProductListDemo.java
@@ -1,13 +1,9 @@
 package app;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import app.dao.ProductDAO;
 import app.entity.Product;
 
 /**
@@ -16,42 +12,17 @@ import app.entity.Product;
 public class ProductListDemo {
 
 	/**
-	 * クラス定数：データベース接続情報文字列群
-	 */
-	private static final String JDBC_DRIVER  = "org.postregsql.Driver"; // Java8より自動で読み込まれるようになったため不要
-	private static final String DB_URL       = "jdbc:postgresql://localhost:5432/jbasicdaodb";
-	private static final String DB_USER      = "student";
-	private static final String DB_PASSWORD  = "himitu";
-	
-	/**
 	 * productsテーブルにアクセスするプログラム
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		// 1. 実行するSQLの設定
-		String sql = "SELECT * FROM products ORDER BY id";
-		
-		try ( // 2.1 データベース接続オブジェクトの取得
-			  Connection  conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			  // 2.2 SQL実行オブジェクトの取得
-			  PreparedStatement pstmt = conn.prepareStatement(sql);
-			  // 2.3 SQLの実行と結果セットの取得
-			  ResultSet rs = pstmt.executeQuery();
-			) {
-			
-			// 3. 結果セットを商品リストに変換
-			List<Product> list= new ArrayList<>();
-			while(rs.next()) {
-				Product entity = new Product();
-				entity.setId(rs.getInt("id"));
-				entity.setName(rs.getString("name"));
-				entity.setPrice(rs.getInt("price"));
-				entity.setStock(rs.getInt("stock"));
-				list.add(entity);
-			}
-			
-			// 4. 商品リストの要素を表示
+		try {
+			// 1. ProductDAOをインスタンス化
+			ProductDAO dao = new ProductDAO();
+			// 2. 商品の全件検索の実行
+			List<Product> list = dao.findAll();
+			// 3. 商品リストの要素を表示
 			System.out.printf("%-6s\t%-10s\t%-6s\t%-3s\n", "商品ID", "商品名", "価格", "在庫数");
 			for (Product product : list) {
 				System.out.printf("%-6s\t%-10s\t%-6s\t%-3s\n", 
@@ -60,7 +31,6 @@ public class ProductListDemo {
 						product.getPrice(),
 						product.getStock());
 			}
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
---
+++ src/app/dao/ProductDAO.java
@@ -0,0 +1,63 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.entity.Product;
+
+/**
+ * productsテーブルにアクセスするDAOクラス
+ */
+public class ProductDAO {
+
+	/**
+	 * クラス定数
+	 */
+	// データベース接続情報文字列群
+	private static final String JDBC_DRIVER  = "org.postregsql.Driver"; // Java8より自動で読み込まれるようになったため不要
+	private static final String DB_URL       = "jdbc:postgresql://localhost:5432/jbasicdaodb";
+	private static final String DB_USER      = "student";
+	private static final String DB_PASSWORD  = "himitu";
+	
+	// SQL文字列定数
+	private static final String SQL_FIND_ALL = "SELECT * FROM products ORDER BY id";
+	
+	/**
+	 * productsテーブルのすべてのレコードを取得する
+	 * @return 商品リスト
+	 * @throws SQLException 
+	 */
+	public List<Product> findAll() throws SQLException {
+		
+		try ( // 2.1 データベース接続オブジェクトの取得
+			  Connection  conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			  // 2.2 SQL実行オブジェクトの取得
+			  PreparedStatement pstmt = conn.prepareStatement(SQL_FIND_ALL);
+			  // 2.3 SQLの実行と結果セットの取得
+			  ResultSet rs = pstmt.executeQuery();
+			) {
+			
+			// 3. 結果セットを商品リストに変換
+			List<Product> list= new ArrayList<>();
+			while(rs.next()) {
+				Product entity = new Product();
+				entity.setId(rs.getInt("id"));
+				entity.setName(rs.getString("name"));
+				entity.setPrice(rs.getInt("price"));
+				entity.setStock(rs.getInt("stock"));
+				list.add(entity);
+			}
+			
+			// 4. 商品リストの返却
+			return list;
+			
+		}
+	}
+	
+	
+}
